62bd5e6b34442ec7bb20a679000eeb69e200493e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-24 22:37:32 +0000

    Deploy mrbalem/NotasVirtuales to github.com/mrbalem/NotasVirtuales.git:master

---
+++ precache-manifest.ff475a6b72b0ee23efce8ed07ed5d7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b47ccfe366f0bbbfb6c73c0fac25acf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "049e1533af3106ae5764",
+    "url": "/static/css/main.0c9d8957.chunk.css"
+  },
+  {
+    "revision": "d80623b491dfe8a6c5bb",
+    "url": "/static/js/2.10f9db1c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.10f9db1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "049e1533af3106ae5764",
+    "url": "/static/js/main.f4f1de85.chunk.js"
+  },
+  {
+    "revision": "74c3a1686d9e04d28970",
+    "url": "/static/js/runtime-main.c5110250.js"
+  }
+]);
